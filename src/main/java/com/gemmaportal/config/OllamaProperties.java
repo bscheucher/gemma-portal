3f11d57c320e385096025f609ed32e85643156e7
bscheucher <bernhard.scheucher@gmail.com>
2026-05-25 21:43:35 +0200

    use lombok @Data annotation

--- src/main/java/com/gemmaportal/config/OllamaProperties.java
+++ src/main/java/com/gemmaportal/config/OllamaProperties.java
@@ -1,26 +1,13 @@
 package com.gemmaportal.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Data
 @ConfigurationProperties(prefix = "ollama")
 public class OllamaProperties {
 
     private String baseUrl = "http://localhost:11434";
     private String model = "gemma3";
 
-    public String getBaseUrl() {
-        return baseUrl;
-    }
-
-    public void setBaseUrl(String baseUrl) {
-        this.baseUrl = baseUrl;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
 }
